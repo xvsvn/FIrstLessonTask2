03fabda5b255c4e7d2abded95414492046effac6
xvsvn <86996400+xvsvn@users.noreply.github.com>
2023-06-07 17:37:09 +0500

    Initial Commit

---
+++ FIrstLessonTask2/FIrstLessonTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  FIrstLessonTask2App.swift
+//  FIrstLessonTask2
+//
+//  Created by Xasan Xasanov on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FIrstLessonTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
